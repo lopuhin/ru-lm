9238b42a57ea019d6e10db2d7420bfaf77595a40
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-10-22 12:48:02 +0300

    Save vocabulary, prediction

--- rnn/predict.py
+++ rnn/predict.py
@@ -1,21 +1,46 @@
 from pathlib import Path
+from typing import List, Tuple
 
 import numpy as np
 import tensorflow as tf
 
+from .reader import load_vocab, tokens_to_ids
+
 
 class Model:
-    def __init__(self, path: Path):
+    def __init__(self, model_path: Path, vocab_path: Path):
+        self.word_to_id = load_vocab(vocab_path)
+        self.id_to_word = {id_: word for word, id_ in self.word_to_id.items()}
         self.session = tf.Session()
-        saver = tf.train.import_meta_graph('{}.meta'.format(path))
-        saver.restore(self.session, str(path))
+        saver = tf.train.import_meta_graph('{}.meta'.format(model_path))
+        saver.restore(self.session, str(model_path))
         self.input_xs = tf.get_collection('input_xs')[0]
         self.batch_size = tf.get_collection('batch_size')[0]
         self.softmax = tf.get_collection('softmax')[0]
+        self.num_steps = 20
 
-    def predict(self):
-        xs = np.zeros([1, 20])
-        xs[0, :3] = [1, 2, 3]
-        softmax = self.session.run(
-            self.softmax, feed_dict={self.input_xs: xs, self.batch_size: 1})
-        return softmax
+    def predict(self, tokens: List[str]) -> np.ndarray:
+        tokens = tokens[-self.num_steps:]
+        xs = np.zeros([1, self.num_steps])
+        n = len(tokens)
+        xs[0, :n] = list(tokens_to_ids(tokens, self.word_to_id))
+        return self.session.run(
+            self.softmax[n - 1],
+            feed_dict={self.input_xs: xs, self.batch_size: 1})
+
+    def predict_top(self, tokens: List[str], top=10) -> List[Tuple[str, float]]:
+        probs = self.predict(tokens)
+        top_indices = argsort_k_largest(probs, top)
+        return [(self.id_to_word[id_], probs[id_]) for id_ in top_indices]
+
+    def __del__(self):
+        if hasattr(self, 'session'):
+            self.session.__exit__(None, None, None)
+
+
+def argsort_k_largest(x, k):
+    if k >= len(x):
+        return np.argsort(x)[::-1]
+    indices = np.argpartition(x, -k)[-k:]
+    values = x[indices]
+    return indices[np.argsort(-values)]
--- rnn/reader.py
+++ rnn/reader.py
@@ -1,9 +1,9 @@
 import collections
+import json
 from pathlib import Path
 from typing import Dict, Iterable, List
 
 import numpy as np
-import tensorflow as tf
 
 
 EOS = '<eos>'
@@ -39,11 +39,26 @@ def _build_vocab(path: Path, vocab_size: int) -> Dict[str, int]:
     return word_to_id
 
 
+def _save_vocab(path: Path, word_to_id: Dict[str, int]):
+    with path.open('wt') as f:
+        json.dump(word_to_id, f,
+                  ensure_ascii=False, sort_keys=True, indent=True)
+
+
+def load_vocab(path) -> Dict[str, int]:
+    with path.open('rt') as f:
+        return json.load(f)
+
+
 def _file_to_word_ids(path: Path, word_to_id: Dict[str, int]) -> np.ndarray:
     data = _words_reader(path)
+    return np.fromiter(tokens_to_ids(data, word_to_id), dtype=np.int32, count=-1)
+
+
+def tokens_to_ids(tokens: Iterable[str], word_to_id: Dict[str, int])\
+        -> Iterable[int]:
     unk = word_to_id.get(UNK)
-    return np.fromiter((word_to_id.get(word, unk) for word in data),
-                       dtype=np.int32, count=-1)
+    return (word_to_id.get(tokens, unk) for tokens in tokens)
 
 
 def load_raw_data(data_path: Path, vocab_size: int):
@@ -76,4 +91,5 @@ def load_raw_data(data_path: Path, vocab_size: int):
             _file_to_word_ids(p, word_to_id) for p in paths]
         for data, path in zip(datas, paths):
             np.save(str(cached(path)), data)
+        _save_vocab(data_path / ('vocab-{}.json'.format(vocab_size)), word_to_id)
     return train_data, valid_data, test_data
